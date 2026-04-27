8b187e4f36cf4f7d167a6300da58d4a386ca1471
OmBikramMalla <itsblackiii11@gmail.com>
2026-04-27 14:14:01 +0545

    Add staff customer API (list + details with history)

--- WebApplications.Application/Interfaces/IServices/ICustomerService.cs
+++ WebApplications.Application/Interfaces/IServices/ICustomerService.cs
@@ -4,6 +4,7 @@ namespace WebApplications.Application.Interfaces.IServices
 {
     public interface ICustomerService
     {
+        Task<List<Customer>> GetAllCustomersAsync();
         Task<Customer?> GetCustomerByIdAsync(long id);
         Task<object?> GetCustomerDetailsWithHistoryAsync(long id);
     }
--- WebApplications.Infrastructure/Services/CustomerService.cs
+++ WebApplications.Infrastructure/Services/CustomerService.cs
@@ -1,6 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApplications.Application.Interfaces.IServices;
 using WebApplications.Domain.Models;
 using WebApplications.Infrastructure.Presistance;
@@ -16,12 +14,17 @@ namespace WebApplications.Infrastructure.Services
             _context = context;
         }
 
+        public async Task<List<Customer>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
         public async Task<Customer?> GetCustomerByIdAsync(long id)
         {
             return await _context.Customers.FindAsync(id);
         }
 
-        public async Task<object> GetCustomerDetailsWithHistoryAsync(long id)
+        public async Task<object?> GetCustomerDetailsWithHistoryAsync(long id)
         {
             var customer = await _context.Customers
                 .Where(c => c.Id == id)
@@ -30,13 +33,14 @@ namespace WebApplications.Infrastructure.Services
                     c.Id,
                     c.FullName,
                     c.Phone,
+                    c.Email,
                     Vehicles = _context.Vehicles.Where(v => v.CustomerId == c.Id).ToList(),
                     Invoices = _context.SalesInvoices.Where(i => i.CustomerId == c.Id).ToList(),
                     Appointments = _context.ServiceAppointments.Where(a => a.CustomerId == c.Id).ToList()
                 })
                 .FirstOrDefaultAsync();
 
-            return customer!;
+            return customer;
         }
     }
 }
